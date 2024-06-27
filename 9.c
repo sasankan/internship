3278ee6cef6db2db644b2b83d3806e0dc9da39f7
Sasankan <sasankan@users.noreply.github.com>
2024-06-27 09:42:58 +0530

    Codeforces problem

---
+++ 9.c
@@ -0,0 +1,43 @@
+// #include<stdio.h>
+// int main(){
+//     int x;
+//     for(x=-1;x<=10;x++){
+//         if(x<5)
+//             continue;
+//         else
+//             break;
+//         printf("bf");                                  
+//     }
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int i=0;
+//     for (; i <=5; i++);
+//         printf("%d",i);
+//     return 0;    
+// }
+
+// #include<stdio.h>
+// int main(){
+//     int a=500,b=100,c;
+//     if(!a>=400)
+//         b=300;
+//     c=200;
+//     printf("b=%dc=%d\n",b,c);
+//     return 0;    
+// }
+
+#include<stdio.h>
+int main(){
+    int x=3;
+    float y=3.0;
+    if(x==y)
+        printf("x and y are equal");
+    else
+        printf("x and y are not equal");
+    return 0;        
+}
+
+
+
