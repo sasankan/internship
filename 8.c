e37bb1d47d2cbcfc54046731b1fbaaf6b4761880
Sasankan <sasankan@users.noreply.github.com>
2024-12-14 09:49:19 +0530

    Number logics

---
+++ 8.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,m,a,b;
+    scanf("%d %d %d %d",&n,&m,&a,&b);
+    if(m*a<b) printf("%d",n*a);
+    else{
+        printf("%d",((n/m)*b)+(((n%m)*a)<b?((n%m)*a):b));
+    }
+
+    return 0;
+}
